ee5dff49c62e3b215357c2be86c4c64c8b37bc46
lowjunkie <meet.tabrezt1@gmail.com>
2021-04-24 02:50:13 +0530

    Finished on the Prototype Mood: __ TODO: __

--- app/src/main/java/com/lj/bwow/app/Dashboard.kt
+++ app/src/main/java/com/lj/bwow/app/Dashboard.kt
@@ -1,10 +1,14 @@
 package com.lj.bwow.app
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.lj.bwow.R
 import com.lj.bwow.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.jar.Manifest
 
 @AndroidEntryPoint
 class Dashboard : AppCompatActivity() {
@@ -13,5 +17,36 @@ class Dashboard : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        requestPermission()
+    }
+
+    private fun hasActivityRecognitionPermission() : Boolean{
+        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission(){
+        var permissionToRequest = mutableListOf<String>()
+        if(!hasActivityRecognitionPermission()){
+            permissionToRequest.add(android.Manifest.permission.ACTIVITY_RECOGNITION)
+        }
+
+        if (permissionToRequest.isNotEmpty()){
+            ActivityCompat.requestPermissions(this, permissionToRequest.toTypedArray(), 0)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 0 && grantResults.isNotEmpty()){
+            for(i in grantResults.indices){
+                if(grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                    Log.d("Permission Requests", grantResults[i].toString())
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/lj/bwow/fragments/FragDashboard.kt
+++ app/src/main/java/com/lj/bwow/fragments/FragDashboard.kt
@@ -40,6 +40,8 @@ class FragDashboard : Fragment(), SensorEventListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDashboardBinding.bind(view)
+        setupPedometer()
+
         viewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
         viewModel.healthData.observe(viewLifecycleOwner, Observer { response ->
             response?.let {
@@ -57,7 +59,7 @@ class FragDashboard : Fragment(), SensorEventListener {
 
     override fun onResume() {
         super.onResume()
-        mSensorManager?.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager?.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_FASTEST)
 
     }
 
@@ -68,13 +70,15 @@ class FragDashboard : Fragment(), SensorEventListener {
     }
 
     private fun setupPedometer(){
-            mSensorManager = getSystemService(requireContext(), SENSOR_SERVICE.javaClass) as SensorManager
+        mSensorManager = requireActivity().getSystemService(SENSOR_SERVICE) as SensorManager
+        if(mSensorManager!=null) {
             mAccelerometer = mSensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
+        }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
         event ?: return
+        Log.d("Shami","Sham")
         // Data 1: According to official documentation, the first value of the `SensorEvent` value is the step count
         event.values.firstOrNull()?.let {
             i +=1
